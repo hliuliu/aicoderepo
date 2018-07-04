6f10e683fcdb5e4f801e370180ad165fcb0ae85c
Haggai Liu <heng_liu94@hotmail.com>
2018-07-03 20:21:45 -0700

    update ai_problem class

--- aiproblem.py
+++ aiproblem.py
@@ -32,7 +32,51 @@ class ai_problem(object):
 			Call this superclass method first for best results
 		'''
 		if action not in self.action_set:
-			raise ValueError("The specified action is not in the list")
+			raise ValueError("The specified action is not in the list: \n"+str(action))
+		if not self.can_take_action(state,action):
+			raise ValueError("The action %s cannot be excecuted from state %s"%(action,state))
 		return state
+
+	def goal_test(self, state):
+		'''
+			the goal test
+			given a state, return whether or not this state is a goal state.
+			return type is bool, value is True or False.
+			Should be overriden in a subclass
+			but defualt, assume that every state is a goal state,
+			so the problem is extremely trivial.
+		'''
+		return True
+
+	def can_take_action(self, state, action):
+		'''
+			returns True or False,
+			depending on if the given action is available from the given state,
+			provided that the action exists globally.
+			Should be overriden in a subclass.
+			by default assume all actions are availiable from every state.
+			Call this superclass method for best results.
+		'''
+		if action not in self.action_set:
+			raise ValueError("The specified action is not in the list: \n"+str(action))
+		return True
+
+	def step_cost(self, state, action):
+		'''
+			the cost of taking the given action from the given state.
+			should return a nonnegative number.
+			should be overriden in a subclass.
+			by default, assume every action has no cost.
+			Call this superclass method for best results.
+		'''
+		if action not in self.action_set:
+			raise ValueError("The specified action is not in the list: \n"+str(action))
+		if not self.can_take_action(state,action):
+			raise ValueError("The action %s cannot be excecuted from state %s"%(action,state))
+		return 0
+
+
+
+
 
 
